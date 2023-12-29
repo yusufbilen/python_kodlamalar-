7337e08fb3e73d0c3fb2a474571d77b20dd07752
yusufbilen <116259109+yusufbilen@users.noreply.github.com>
2023-12-29 10:46:04 +0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,24 +1,24 @@
-# class ogrenci:
-#     def _init_(self,isim,soyad,vize,final):
-#         self.ad=isim
-#         self.soyisim=soyad
-#         self.not1=vize
-#         self.not2=final
-#     def hesap(self):
-#         ortalama = (self.not1*0.4)+ (self.not2*0.6) 
-#         if (ortalama>=85):
-#             print("Harf notunuz : AA0")
-#         elif(ortalama>=70 and ortalama <85):
-#             print("Harf notunuz : BA")
+class ogrenci:
+    def _init_(self,isim,soyad,vize,final):
+        self.ad=isim
+        self.soyisim=soyad
+        self.not1=vize
+        self.not2=final
+    def hesap(self):
+        ortalama = (self.not1*0.4)+ (self.not2*0.6) 
+        if (ortalama>=85):
+            print("Harf notunuz : AA0")
+        elif(ortalama>=70 and ortalama <85):
+            print("Harf notunuz : BA")
  
-#         elif(ortalama>=60 and ortalama <70):
-#             print("Harf notunuz : BB")
+        elif(ortalama>=60 and ortalama <70):
+            print("Harf notunuz : BB")
  
-#         elif (ortalama >= 45 and ortalama < 60):
-#             print("Harf notunuz : CB")
+        elif (ortalama >= 45 and ortalama < 60):
+            print("Harf notunuz : CB")
  
-#         elif(ortalama>=0 and ortalama <45):
-#             print("Harf notunuz : FF")
+        elif(ortalama>=0 and ortalama <45):
+            print("Harf notunuz : FF")
 
 def selection_sort(liste):
     for i in range(0,len(liste)-1):
