e7ae7f1d3759642a2ae3b79d65ca6c8a9a3c5bcb
yusufbilen <116259109+yusufbilen@users.noreply.github.com>
2023-12-29 10:46:22 +0300

    Update istatistik.py

--- istatistik.py
+++ istatistik.py
@@ -1,44 +1,44 @@
-# x=[]
-# y=[]
-# z=[]
-# c=[]
+x=[]
+y=[]
+z=[]
+c=[]
 
-# while True:
-#     deger=(input('x listesi iççin değer giriniz: '))
-#     if deger=="":
-#         break
-#     else:
-#         x.append(float(deger))
-#         print(f"x listesi girildi {x} ")
+while True:
+    deger=(input('x listesi iççin değer giriniz: '))
+    if deger=="":
+        break
+    else:
+        x.append(float(deger))
+        print(f"x listesi girildi {x} ")
 
-# for i in range(len(x)):
-#     deger=input("y değeri için değer giriniz: ")
-#     y.append(float(deger))
-# print(f"y listesi girildi {y}")
+for i in range(len(x)):
+    deger=input("y değeri için değer giriniz: ")
+    y.append(float(deger))
+print(f"y listesi girildi {y}")
 
-# sumx=sum(x)
-# sumx2=0
+sumx=sum(x)
+sumx2=0
 
-# for i in x:
-#     sumx2=sumx2+(i**2)
+for i in x:
+    sumx2=sumx2+(i**2)
 
-# sumy=sum(y)
-# sumy2=0
-# for i in y:
-#     sumy2=sumy2+(i**2)
+sumy=sum(y)
+sumy2=0
+for i in y:
+    sumy2=sumy2+(i**2)
 
-# sumxy=0
-# for i in range(len(x)):
-#     sumxy=sumxy+(x[i]*y[i])    
-# sxx=sumx2-(sumx**2/len(x))
-# syy=sumy2-(sumy**2/len(y))
-# sxy=sumxy-(sumx*sumy/len(x))
+sumxy=0
+for i in range(len(x)):
+    sumxy=sumxy+(x[i]*y[i])    
+sxx=sumx2-(sumx**2/len(x))
+syy=sumy2-(sumy**2/len(y))
+sxy=sumxy-(sumx*sumy/len(x))
 
-# import math
-# b=math.sqrt(sxx)*math.sqrt(syy)
+import math
+b=math.sqrt(sxx)*math.sqrt(syy)
 
-# r=sxy/b
-# print(r)
+r=sxy/b
+print(r)
 
 
 
